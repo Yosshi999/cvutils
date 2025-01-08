9d990d80b1709afb23d9c9fe6d97ef55190feb3f
Yosshi999 <Yosshi999@users.noreply.github.com>
2025-01-09 01:29:40 +0900

    fix lint

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import Image from "next/image";
 import { Jimp } from "jimp";
 import { useState, useRef, useEffect } from "react";
 import { Tensor, InferenceSession } from 'onnxruntime-web';
